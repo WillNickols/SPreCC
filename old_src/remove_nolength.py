079a69e1a2d07e8090d0b4eb5a93ab84c752b06a
Will Nickols <78048944+WillNickols@users.noreply.github.com>
2022-08-16 16:21:35 -0500

    Delete old_src directory

--- old_src/remove_nolength.py
+++
@@ -1,4 +0,0 @@
-from Bio import SeqIO
-for current_seq in SeqIO.parse("clustering/fasta_merged/fasta_merged.fasta", "fasta"):
-    if current_seq.seq != '' and len(current_seq.seq)<1000:
-        print (">" + current_seq.id + "\n" + current_seq.seq)
