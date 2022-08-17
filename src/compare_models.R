a524ed2640209048965a9ab7810b457379caca0b
Will Nickols <willnickols@college.harvard.edu>
2022-08-17 13:03:42 -0500

    Updated figures

--- src/compare_models.R
+++ src/compare_models.R
@@ -34,7 +34,7 @@ models_from_weights <- function() {
   p1 <-
     ggplot() +
     xlim(0, 1) +
-    ylim(0,8)
+    ylim(0,9)
   
   for(i in 1:nrow(model_list[[1]])) {
     p1 <- p1 + geom_function(fun = sigmoid, args = list(w_0 = model_list[[1]]$w_0_bin[i], 
@@ -125,13 +125,14 @@ models_from_weights <- function() {
 }
 
 n_p_histogram <- function(){
-  test_n_p <- read.table("train_outputs/non_model/test_n_p.tsv", 
+  val_n_p <- read.table("train_outputs/non_model/val_n_p.tsv", 
              header = T, sep = "\t", check.names = F)
-  test_n_p$partition <- paste0("Test\n(", round(100 * mean(test_n_p$n_p==0), 2), "% 0)")
+  val_n_p$partition <- paste0("Validation\n(", round(100 * mean(val_n_p$n_p==0), 2), "% 0)")
   train_n_p <- read.table("train_outputs/non_model/train_n_p.tsv", 
                          header = T, sep = "\t", check.names = F)
   train_n_p$partition <- paste0("Train\n(", round(100*mean(train_n_p$n_p==0),2), "% 0)")
-  merged <- rbind(test_n_p, train_n_p)
+  merged <- rbind(val_n_p, train_n_p)
+  merged$partition <- factor(merged$partition, levels = c(paste0("Validation\n(", round(100 * mean(val_n_p$n_p==0), 2), "% 0)"), paste0("Train\n(", round(100*mean(train_n_p$n_p==0),2), "% 0)")))
   
   dir.create(file.path("Figures/n_p/"), showWarnings = FALSE)
   png(file=paste0("Figures/n_p/n_p_by_partition.png"), width = 2400, height = 700)
@@ -153,7 +154,7 @@ density_interval_vs_nominal <- function(){
                               header = T, sep = "\t", check.names = F)
     df_addition <- data.frame("capture_prop"=train_out$`mean capture proportion`,
           "nominal" = gsub(".*ci_", "", file_list[i]) %>% gsub(pattern="[^0-9].*", replacement="") %>% as.numeric(),
-          "partition" = ifelse(grepl("val", file_list[i]), "Test", "Train"),
+          "partition" = ifelse(grepl("val", file_list[i]), "Validation", "Train"),
           "n_p" = ifelse(grepl("only_sim", file_list[i]), "first", "second"),
           "model" = gsub("_.*", "", file_list[i]))
     density_interval_df <- rbind(density_interval_df, df_addition)
@@ -161,7 +162,7 @@ density_interval_vs_nominal <- function(){
   }
   colnames(density_interval_df) <- c("capture_prop", "nominal", "partition", "n_p", "model")
   density_interval_df$nominal <- factor(density_interval_df$nominal)
-  density_interval_df$partition <- factor(density_interval_df$partition, levels=c("Train", "Test"))
+  density_interval_df$partition <- factor(density_interval_df$partition, levels=c("Train", "Validation"))
   density_interval_df$n_p <- factor(density_interval_df$n_p)
   density_interval_df$model <- case_when(density_interval_df$model == "model1" ~ "Model 1",
                                          density_interval_df$model == "model2" ~ "Model 2",
@@ -196,19 +197,19 @@ volcano_plot_mean <- function(){
                             header = T, sep = "\t", check.names = F)
     df_addition <- data.frame("coef"=train_out$`mean coef 1`,
                               "signif" = train_out$`mean signif 1`,
-                              "partition" = ifelse(grepl("val", file_list[i]), "Test", "Train"),
+                              "partition" = ifelse(grepl("val", file_list[i]), "Validation", "Train"),
                               "n_p" = ifelse(grepl("only_sim", file_list[i]), "first", "second"),
                               "model" = gsub("_.*", "", file_list[i]))
     volcano_df <- rbind(volcano_df, df_addition)
     df_addition <- data.frame("coef"=train_out$`mean coef 2`,
                               "signif" = train_out$`mean signif 2`,
-                              "partition" = ifelse(grepl("val", file_list[i]), "Test", "Train"),
+                              "partition" = ifelse(grepl("val", file_list[i]), "Validation", "Train"),
                               "n_p" = ifelse(grepl("only_sim", file_list[i]), "first", "second"),
                               "model" = gsub("_.*", "", file_list[i]))
     volcano_df <- rbind(volcano_df, df_addition)
   }
   colnames(volcano_df) <- c("coef", "signif", "partition", "n_p", "model")
-  volcano_df$partition <- factor(volcano_df$partition, levels=c("Train", "Test"))
+  volcano_df$partition <- factor(volcano_df$partition, levels=c("Train", "Validation"))
   volcano_df$n_p <- factor(volcano_df$n_p)
   volcano_df$model <- case_when(volcano_df$model == "model1" ~ "Model 1",
                                 volcano_df$model == "model2" ~ "Model 2",
@@ -244,19 +245,19 @@ volcano_plot_mode <- function(){
                             header = T, sep = "\t", check.names = F)
     df_addition <- data.frame("coef"=train_out$`mode coef 1`,
                               "signif" = train_out$`mode signif 1`,
-                              "partition" = ifelse(grepl("val", file_list[i]), "Test", "Train"),
+                              "partition" = ifelse(grepl("val", file_list[i]), "Validation", "Train"),
                               "n_p" = ifelse(grepl("only_sim", file_list[i]), "first", "second"),
                               "model" = gsub("_.*", "", file_list[i]))
     volcano_df <- rbind(volcano_df, df_addition)
     df_addition <- data.frame("coef"=train_out$`mode coef 2`,
                               "signif" = train_out$`mode signif 2`,
-                              "partition" = ifelse(grepl("val", file_list[i]), "Test", "Train"),
+                              "partition" = ifelse(grepl("val", file_list[i]), "Validation", "Train"),
                               "n_p" = ifelse(grepl("only_sim", file_list[i]), "first", "second"),
                               "model" = gsub("_.*", "", file_list[i]))
     volcano_df <- rbind(volcano_df, df_addition)
   }
   colnames(volcano_df) <- c("coef", "signif", "partition", "n_p", "model")
-  volcano_df$partition <- factor(volcano_df$partition, levels=c("Train", "Test"))
+  volcano_df$partition <- factor(volcano_df$partition, levels=c("Train", "Validation"))
   volcano_df$n_p <- factor(volcano_df$n_p)
   volcano_df$model <- case_when(volcano_df$model == "model1" ~ "Model 1",
                                 volcano_df$model == "model2" ~ "Model 2",
@@ -292,19 +293,19 @@ volcano_plot_median <- function(){
                             header = T, sep = "\t", check.names = F)
     df_addition <- data.frame("coef"=train_out$`median coef 1`,
                               "signif" = train_out$`median signif 1`,
-                              "partition" = ifelse(grepl("val", file_list[i]), "Test", "Train"),
+                              "partition" = ifelse(grepl("val", file_list[i]), "Validation", "Train"),
                               "n_p" = ifelse(grepl("only_sim", file_list[i]), "first", "second"),
                               "model" = gsub("_.*", "", file_list[i]))
     volcano_df <- rbind(volcano_df, df_addition)
     df_addition <- data.frame("coef"=train_out$`median coef 2`,
                               "signif" = train_out$`median signif 2`,
-                              "partition" = ifelse(grepl("val", file_list[i]), "Test", "Train"),
+                              "partition" = ifelse(grepl("val", file_list[i]), "Validation", "Train"),
                               "n_p" = ifelse(grepl("only_sim", file_list[i]), "first", "second"),
                               "model" = gsub("_.*", "", file_list[i]))
     volcano_df <- rbind(volcano_df, df_addition)
   }
   colnames(volcano_df) <- c("coef", "signif", "partition", "n_p", "model")
-  volcano_df$partition <- factor(volcano_df$partition, levels=c("Train", "Test"))
+  volcano_df$partition <- factor(volcano_df$partition, levels=c("Train", "Validation"))
   volcano_df$n_p <- factor(volcano_df$n_p)
   volcano_df$model <- case_when(volcano_df$model == "model1" ~ "Model 1",
                                 volcano_df$model == "model2" ~ "Model 2",
@@ -340,14 +341,14 @@ auc_eval <- function(){
                             header = T, sep = "\t", check.names = F)
     df_addition <- data.frame("auc"=train_out$auc,
                               "f1" = train_out$f1,
-                              "partition" = ifelse(grepl("val", file_list[i]), "Test", "Train"),
+                              "partition" = ifelse(grepl("val", file_list[i]), "Validation", "Train"),
                               "n_p" = ifelse(grepl("only_sim", file_list[i]), "first", "second"),
                               "model" = gsub("_.*", "", file_list[i]))
     auc_df <- rbind(auc_df, df_addition)
     
   }
   colnames(auc_df) <- c("auc", "f1", "partition", "n_p", "model")
-  auc_df$partition <- factor(auc_df$partition, levels=c("Train", "Test"))
+  auc_df$partition <- factor(auc_df$partition, levels=c("Train", "Validation"))
   auc_df$n_p <- factor(auc_df$n_p)
   auc_df$model <- case_when(auc_df$model == "model1" ~ "Model 1",
                             auc_df$model == "model2" ~ "Model 2",
@@ -396,14 +397,14 @@ weighed_metrics <- function(){
     df_addition <- data.frame("weighted_f1"=sum(train_out$f1 * props, na.rm = T) / sum(props, na.rm = T),
                               "weighted_precision"=sum(train_out$precision * props, na.rm = T) / sum(props, na.rm = T),
                               "weighted_recall"=sum(train_out$recall * props, na.rm = T) / sum(props, na.rm = T),
-                              "partition" = ifelse(grepl("val", file_list[i]), "Test", "Train"),
+                              "partition" = ifelse(grepl("val", file_list[i]), "Validation", "Train"),
                               "n_p" = ifelse(grepl("only_sim", file_list[i]), "first", "second"),
                               "model" = gsub("_.*", "", file_list[i]))
     weighted_df <- rbind(weighted_df, df_addition)
     
   }
   colnames(weighted_df) <- c("f1", "precision", "recall", "partition", "n_p", "model")
-  weighted_df$partition <- factor(weighted_df$partition, levels=c("Train", "Test"))
+  weighted_df$partition <- factor(weighted_df$partition, levels=c("Train", "Validation"))
   weighted_df$n_p <- factor(weighted_df$n_p)
   weighted_df$model <- case_when(weighted_df$model == "model1" ~ "Model 1",
                             weighted_df$model == "model2" ~ "Model 2",
@@ -456,14 +457,14 @@ mean_abs_err <- function(){
     train_out <- read.table(paste0("train_outputs/analysis/", file_list[i]), 
                             header = T, sep = "\t", check.names = F)
     df_addition <- data.frame("mean_abs_err"=c(train_out$`mean abs err 1`, train_out$`mean abs err 2`[!is.na(train_out$`mean abs err 2`)]) / sds,
-                              "partition" = ifelse(grepl("val", file_list[i]), "Test", "Train"),
+                              "partition" = ifelse(grepl("val", file_list[i]), "Val", "Train"),
                               "n_p" = ifelse(grepl("only_sim", file_list[i]), "first", "second"),
                               "model" = gsub("_.*", "", file_list[i]))
     weighted_df <- rbind(weighted_df, df_addition)
     
   }
   colnames(weighted_df) <- c("mean_abs_err", "partition", "n_p", "model")
-  weighted_df$partition <- factor(weighted_df$partition, levels=c("Train", "Test"))
+  weighted_df$partition <- factor(weighted_df$partition, levels=c("Train", "Val"))
   weighted_df$n_p <- factor(weighted_df$n_p)
   weighted_df$model <- case_when(weighted_df$model == "model1" ~ "Model 1",
                                  weighted_df$model == "model2" ~ "Model 2",
@@ -505,14 +506,14 @@ median_abs_err <- function(){
     train_out <- read.table(paste0("train_outputs/analysis/", file_list[i]), 
                             header = T, sep = "\t", check.names = F)
     df_addition <- data.frame("median_abs_err"=c(train_out$`median abs err 1`, train_out$`median abs err 2`[!is.na(train_out$`median abs err 2`)]) / sds,
-                              "partition" = ifelse(grepl("val", file_list[i]), "Test", "Train"),
+                              "partition" = ifelse(grepl("val", file_list[i]), "Val", "Train"),
                               "n_p" = ifelse(grepl("only_sim", file_list[i]), "first", "second"),
                               "model" = gsub("_.*", "", file_list[i]))
     weighted_df <- rbind(weighted_df, df_addition)
     
   }
   colnames(weighted_df) <- c("median_abs_err", "partition", "n_p", "model")
-  weighted_df$partition <- factor(weighted_df$partition, levels=c("Train", "Test"))
+  weighted_df$partition <- factor(weighted_df$partition, levels=c("Train", "Val"))
   weighted_df$n_p <- factor(weighted_df$n_p)
   weighted_df$model <- case_when(weighted_df$model == "model1" ~ "Model 1",
                                  weighted_df$model == "model2" ~ "Model 2",
@@ -554,14 +555,14 @@ mode_abs_err <- function(){
     train_out <- read.table(paste0("train_outputs/analysis/", file_list[i]), 
                             header = T, sep = "\t", check.names = F)
     df_addition <- data.frame("mode_abs_err"=c(train_out$`mode abs err 1`, train_out$`mode abs err 2`[!is.na(train_out$`mode abs err 2`)]) / sds,
-                              "partition" = ifelse(grepl("val", file_list[i]), "Test", "Train"),
+                              "partition" = ifelse(grepl("val", file_list[i]), "Val", "Train"),
                               "n_p" = ifelse(grepl("only_sim", file_list[i]), "first", "second"),
                               "model" = gsub("_.*", "", file_list[i]))
     weighted_df <- rbind(weighted_df, df_addition)
     
   }
   colnames(weighted_df) <- c("mode_abs_err", "partition", "n_p", "model")
-  weighted_df$partition <- factor(weighted_df$partition, levels=c("Train", "Test"))
+  weighted_df$partition <- factor(weighted_df$partition, levels=c("Train", "Val"))
   weighted_df$n_p <- factor(weighted_df$n_p)
   weighted_df$model <- case_when(weighted_df$model == "model1" ~ "Model 1",
                                  weighted_df$model == "model2" ~ "Model 2",
@@ -604,14 +605,14 @@ density_interval <- function(){
                             header = T, sep = "\t", check.names = F)
     df_addition <- data.frame("density_interval"=c(train_out$`mean CI width 1`, train_out$`mean CI width 2`[!is.na(train_out$`mean CI width 2`)]) / sds,
                               "nominal" = gsub(".*ci_", "", file_list[i]) %>% gsub(pattern="[^0-9].*", replacement="") %>% as.numeric(),
-                              "partition" = ifelse(grepl("val", file_list[i]), "Test", "Train"),
+                              "partition" = ifelse(grepl("val", file_list[i]), "Val", "Train"),
                               "n_p" = ifelse(grepl("only_sim", file_list[i]), "first", "second"),
                               "model" = gsub("_.*", "", file_list[i]))
     weighted_df <- rbind(weighted_df, df_addition)
     
   }
   colnames(weighted_df) <- c("density_interval", "nominal", "partition", "n_p", "model")
-  weighted_df$partition <- factor(weighted_df$partition, levels=c("Train", "Test"))
+  weighted_df$partition <- factor(weighted_df$partition, levels=c("Train", "Val"))
   weighted_df$n_p <- factor(weighted_df$n_p)
   weighted_df$nominal <- paste0(weighted_df$nominal, "% Density interval")
   weighted_df$model <- case_when(weighted_df$model == "model1" ~ "Model 1",
